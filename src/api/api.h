d2c854baf64d16c0b032d3c4d42fc3a616f2b07c
Radek Vais <vais.radek@gmail.com>
2017-11-06 21:41:57 +0100

    Hotfix api.h  - resolve conflict with macro stout, stdin, stderr

--- src/api/api.h
+++ src/api/api.h
@@ -54,7 +54,7 @@ struct TDir_Entry {
 
 struct TProcess_Startup_Info {
 	char *arg;			//pointer na parametry
-	THandle stdin, stdout, stderr;	//kazdy, ktery bude erInvalid_Handle, bude mit vychozi hodnotu
+	THandle OSstdin, OSstdout, OSstderr;	//kazdy, ktery bude erInvalid_Handle, bude mit vychozi hodnotu
 };
 
 /*
