732bf73eeeef1eb0f9bae714df8765e6c4641946
Radek Vais <vais.radek@gmail.com>
2017-11-06 22:53:12 +0100

    Global process structures (variables), basic handle switch to routines, getPID()

--- src/kernel/process.cpp
+++ src/kernel/process.cpp
@@ -1,2 +1,58 @@
 #include "process.h"
 
+/// /////////////////////////////////////////////////////////////////////////////////////////
+/// Interface funnction
+/// Function handle and call routines connected with creating process
+/// <param name='context'>Processor context prepared for system call</param>
+/// <return>Success flag</return>
+bool HandleProcess(kiv_os::TRegisters &context)
+{
+		switch (context.rax.l)
+		{
+			case kiv_os::scClone:
+				routineCloneProcess(context);
+			case kiv_os::scWait_For:
+				routineWaitForProcess(context);
+			default:
+				//todo throw not implemented err
+				break;
+		}
+		return false;
+}
+
+/// /////////////////////////////////////////////////////////////////////////////////////////
+/// Routine create new process
+///<param name='context'>Reference to processor registers</param>
+/// <return>Success flag</return>
+bool routineCloneProcess(kiv_os::TRegisters & context)
+{
+	//TODO not implemented yet
+	return false;
+}
+
+
+/// /////////////////////////////////////////////////////////////////////////////////////////
+/// Routine create new thread
+///<param name='context'>Reference to processor registers</param>
+/// <return>Success flag</return>
+bool routineWaitForProcess(kiv_os::TRegisters & context)
+{
+	//TODO not implemented yet
+	return false;
+}
+
+
+/// /////////////////////////////////////////////////////////////////////////////////////////
+/// Function look up PID in map. Using current thread ID instead of using HW scheduler values.
+/// <return>PID of running process -1 if error occured</return>
+kiv_os::THandle getPid()
+{
+	kiv_os::THandle pid = -1;
+	std::thread::id this_id = std::this_thread::get_id();
+
+	tid_map_lock.lock();
+	pid = tid_to_pid[this_id];
+	tid_map_lock.unlock();
+
+	return pid;
+}
--- src/kernel/process.h
+++ src/kernel/process.h
@@ -3,17 +3,44 @@
 #include <vector>
 #include <string>
 #include <thread>
-
+#include <mutex>
+#include <map>
 
 #include "../api/api.h"
 
-size_t next_pid_value = 0;
-
-typedef struct program_control_block {
-	size_t pid;
-	kiv_os::TRegisters register_state;
+/// One line from process control block table
+struct PCB {
+	kiv_os::THandle pid;
+	std::thread th;
+	std::string program_name;
 	std::vector<kiv_os::THandle> io_devices;
 	std::string working_directory;
 
-	std::thread th;
-} PCB ;
+	kiv_os::TRegisters register_state; // not needed??
+};
+
+/// global variable for next PID (Use in critical section only of PCB table !!!)
+kiv_os::THandle next_pid_value = 0;
+
+/// Lock for critical section of PCB Table
+std::mutex process_table_lock;
+
+/// Lock for critical section of tid to pid map
+std::mutex tid_map_lock;
+
+/// Table of running processes
+std::vector<PCB> process_table;
+
+/// Map of TID (simulator machine) to PID (simulated in OS)
+std::map<std::thread::id , kiv_os::THandle> tid_to_pid;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Interface funnction
+bool HandleProcess(kiv_os::TRegisters &context);
+
+// Working routines
+bool routineCloneProcess(kiv_os::TRegisters &context);
+bool routineWaitForProcess(kiv_os::TRegisters &context);
+
+kiv_os::THandle getPid();
