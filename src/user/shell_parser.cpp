b8f83910f60f4ea88afd298994ca6878d025c331
Jaroslav Klaus <buzz3333@gmail.com>
2017-11-12 20:51:03 +0100

    Add std_err handling in parser

--- src/user/shell_parser.cpp
+++ src/user/shell_parser.cpp
@@ -4,19 +4,21 @@
 
 #include "shell_parser.h"
 
-kiv_os::Command createCommand(std::string command, std::vector<std::string> parameters, kiv_os::Type *out, kiv_os::Type *in) {
+kiv_os::Command createCommand(std::string command, std::vector<std::string> parameters, kiv_os::InOutType *in, kiv_os::InOutType *out, kiv_os::InOutType *err) {
 	kiv_os::Command retVal;
 	retVal.name = command;
 	retVal.parameters = parameters;
 	retVal.std_in = *in;
 	retVal.std_out = *out;
+	retVal.std_err = *err;
 	// TODO: Klaus - What should std_err be? Same as out unless specified?
-	if (*out == kiv_os::Type::PIPE) {
-		*in = kiv_os::Type::PIPE;
+	if (*out == kiv_os::InOutType::PIPE) {
+		*in = kiv_os::InOutType::PIPE;
 	} else {
-		*in = kiv_os::Type::STANDARD;
+		*in = kiv_os::InOutType::STANDARD;
 	}
-	*out = kiv_os::Type::STANDARD;
+	*out = kiv_os::InOutType::STANDARD;
+	*err = kiv_os::InOutType::STANDARD;
 
 	return retVal;
 }
@@ -26,27 +28,39 @@ std::vector<kiv_os::Command> splitToCommands(std::vector<std::string> parts) {
 	std::vector<std::string> params = std::vector<std::string>();
 	std::string command;
 	size_t size = parts.size();
-	kiv_os::Type out = kiv_os::Type::STANDARD;
-	kiv_os::Type in = kiv_os::Type::STANDARD;
+	kiv_os::InOutType in = kiv_os::InOutType::STANDARD;
+	kiv_os::InOutType out = kiv_os::InOutType::STANDARD;
+	kiv_os::InOutType err = kiv_os::InOutType::STANDARD;
 
 	for (int i = 0; i < size; i++) {
 		std::string p = parts[i];
-		// TODO: Klaus - Handle redirects
 		if (p == "echo" || p == "cd" || p == "dir" || p == "md" || p == "rd" || p == "type" || p == "wc" || p == "sort" || p == "ps" || p == "rgen" || p == "freq" || p == "shutdown") {
 			if (i != 0) {
-				retVal.push_back(createCommand(command, params, &out, &in));
+				retVal.push_back(createCommand(command, params, &in, &out, &err));
 				params.clear();
 			}
 			command = p;
 		}
 		else if (p == "|") {
-			out = kiv_os::Type::PIPE;
+			out = kiv_os::InOutType::PIPE;
+		} else if (p == ">") {
+			out = kiv_os::InOutType::FILE_NEW;
+		} else if (p == ">>") {
+			out = kiv_os::InOutType::FILE_APPEND;
+		} else if (p == "<") {
+			in = kiv_os::InOutType::FILE_NEW;
+		} else if (p == "2>") {
+			err = kiv_os::InOutType::FILE_NEW;
+		} else if (p == "2>>") {
+			err = kiv_os::InOutType::FILE_APPEND;
 		} else {
+			// TODO: Klaus - Handle parameters in quotes.
 			params.push_back(p);
 		}
 	}
+	// Push last command to vector.
 	if (size > 0) {
-		retVal.push_back(createCommand(command, params, &out, &in));
+		retVal.push_back(createCommand(command, params, &in, &out, &err));
 	}
 
 	return retVal;
--- src/user/shell_parser.h
+++ src/user/shell_parser.h
@@ -5,19 +5,19 @@
 
 namespace kiv_os {
 
-	// TODO: Klaus - Consider renaming.
-	enum class Type {
+	enum class InOutType {
 		STANDARD,
 		PIPE,
-		FILE
+		FILE_NEW,
+		FILE_APPEND
 	};
 
 	typedef struct shell_command {
 		std::string name;
 		std::vector<std::string> parameters;
-		Type std_in;
-		Type std_out;
-		Type std_err;
+		InOutType std_in;
+		InOutType std_out;
+		InOutType std_err;
 	} Command;
 
 	std::vector<kiv_os::Command> parseLine(std::string line);
