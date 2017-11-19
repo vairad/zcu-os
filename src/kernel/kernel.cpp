2b995cac4d33bf9dd4656a8ccb2be6f34bfb8c2b
Radek Vais <vais.radek@gmail.com>
2017-11-19 22:14:37 +0100

    Dummy testing function for run process

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -42,13 +42,40 @@ void __stdcall Sys_Call(kiv_os::TRegisters &regs)
 }
 
 
+void runProcess()
+{
+	kiv_os::TRegisters regs;
+	regs.rax.h = kiv_os::scProc;
+	regs.rax.l = kiv_os::scClone;
+	regs.rcx.x = kiv_os::clCreate_Process;
+
+	char * program = "md";
+
+	regs.rdx.r = (uint64_t)program;
+
+	kiv_os::TProcess_Startup_Info procInfo;
+	procInfo.OSstderr = kiv_os::erInvalid_Handle;
+	procInfo.OSstdin = kiv_os::erInvalid_Handle;
+	procInfo.OSstdout = kiv_os::erInvalid_Handle;
+	procInfo.arg = "no args";
+
+	regs.rdi.r = (uint64_t) &procInfo;
+
+	Sys_Call(regs);
+
+	regs.rax.r = (uint64_t)0;
+}
+
 /// ///////////////////////////////////////////////////////////////////
 ///  Kernel entry point
 ///
 void __stdcall Run_VM() {
 	Initialize_Kernel();
 
+	runProcess();
+
 	//spustime shell - v realnem OS bychom ovsem spousteli login
+
 	kiv_os::TEntry_Point shell = (kiv_os::TEntry_Point)GetProcAddress(User_Programs, "shell");
 	if (shell) {
 		//spravne se ma shell spustit pres clone!
