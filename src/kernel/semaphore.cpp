9c995ed71024a54b266f3c621d45e24f1752ce09
Thoronir42 <Thoronir42@Gmail.com>
2017-11-27 13:17:59 +0100

    Basic semaphore implementation

---
+++ src/kernel/semaphore.cpp
@@ -0,0 +1,31 @@
+#include "semaphore.h"
+
+semaphore::semaphore::semaphore(const size_t initialValue)
+		: waiting_count(0)
+		, semaphore_value(initialValue)
+{
+}
+
+void semaphore::semaphore::acquire()
+{
+	std::unique_lock<std::mutex> lck(cond_lock);
+	if(semaphore_value <= 0)
+	{
+		waiting_count++;
+		waiting.wait(lck);
+		return;
+	}
+	semaphore_value--;
+}
+
+void semaphore::semaphore::release()
+{
+	std::unique_lock<std::mutex> lck(cond_lock);
+	if(waiting_count > 0)
+	{
+		waiting_count--;
+		waiting.notify_one();
+		return;
+	}
+	semaphore_value++;
+}
---
+++ src/kernel/semaphore.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <condition_variable>
+
+namespace semaphore
+{
+	class semaphore
+	{
+		size_t waiting_count;
+		std::condition_variable waiting;
+		std::mutex	cond_lock;
+		size_t semaphore_value;
+
+	public:
+		/**
+		 * \brief Semaphore constructor
+		 * \param initialValue number of acquires before semaphore blocks
+		 */
+		explicit semaphore(size_t initialValue);
+		
+		/**
+		 * \brief Semaphore operation P() decrease semaphore value.
+		 * If semaphore value is zero next call will sleep thread.
+		 */
+		void acquire(); 
+
+		/**
+		 * \brief Semaphore operation V() increase semaphore value.
+		 * If semaphore value was zero wake one from wating queue.
+		 */
+		void release();
+	};
+}
