c55046cf8b4ff7635ea445caca52c68753468f7e
Jaroslav Klaus <buzz3333@gmail.com>
2017-11-30 00:39:42 +0100

    Add md function prototype

--- src/user/md.cpp
+++ src/user/md.cpp
@@ -1,10 +1,27 @@
+#include "common.h"
+#include "rtl.h"
 
 #undef stdin
 #undef stderr
 #undef stdout
 #include "..\api\api.h"
 
+size_t md_main(int argc, char **argv) {
+	if (argc == 1) {
+		char *file = argv[0];
+		kiv_os::THandle handle = kiv_os_rtl::Create_File(file, 0);
+		// TODO: Klaus - Check for error in all application where creating file.
+	} else {
+		// TODO: Klaus - Error - wrong number of parameters.
+	}
+
+	return 0;
+}
+
 size_t __stdcall md(const kiv_os::TRegisters &regs) 
 {
+	int argc;
+	char **argv = kiv_os::getArgs(regs, &argc);
+	md_main(argc, argv);
 	return 0;
 }
