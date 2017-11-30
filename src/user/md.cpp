c2ccd8ff69a82f0f4c9d32828f45a8fbf2e03750
Jaroslav Klaus <buzz3333@gmail.com>
2017-11-30 22:10:28 +0100

    Fix creating a directory instead of a file, close #35

--- src/user/md.cpp
+++ src/user/md.cpp
@@ -9,8 +9,9 @@
 size_t md_main(int argc, char **argv) {
 	if (argc == 1) {
 		char *file = argv[0];
-		kiv_os::THandle handle = kiv_os_rtl::Create_File(file, 0);
-		// TODO: Klaus - Check for error in all application where creating file.
+		kiv_os::THandle handle = kiv_os_rtl::Create_File(file, kiv_os::faDirectory);
+		// TODO: Klaus - Check for error all over the application where creating file.
+		kiv_os_rtl::Close_File(handle);
 	} else {
 		// Error - wrong number of parameters.
 		std::string error = "The syntax of the command is incorrect.";
