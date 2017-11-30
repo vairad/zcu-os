9c51d6a97b4e648322ab54be75a09ba0e3ccb274
Jaroslav Klaus <buzz3333@gmail.com>
2017-11-30 20:51:45 +0100

    Add type prototype, fix md error

--- src/user/type.cpp
+++ src/user/type.cpp
@@ -1,11 +1,42 @@
-
+#include "common.h"
+#include "rtl.h"
 
 #undef stdin
 #undef stderr
 #undef stdout
 #include "..\api\api.h"
 
+void type_main(int argc, char **argv) {
+	if (argc > 0) {
+		for (size_t i = 0; i < argc; i++) {
+			kiv_os::THandle file = kiv_os_rtl::Create_File(argv[i], kiv_os::fmOpen_Always);
+			if (file == kiv_os::erInvalid_Handle) {
+				// TODO: Klaus - Handle error.
+				continue;
+			}
+
+			size_t read;
+			char buffer[1024];
+			bool ok = kiv_os_rtl::Read_File(file, &buffer, sizeof(buffer) - 1, read);
+			buffer[read] = 0; //Terminate the string.
+			if (!ok) {
+				// TODO: Klaus - Handle error.
+				continue;
+			}
+
+			size_t written = kiv_os::print(buffer, read);
+		}
+	} else {
+		// Error - wrong number of parameters.
+		std::string error = "The syntax of the command is incorrect.";
+		kiv_os::printErr(error.c_str(), error.length());
+	}
+}
+
 size_t __stdcall type(const kiv_os::TRegisters &regs)
 { 
+	int argc;
+	char **argv = kiv_os::getArgs(regs, &argc);
+	type_main(argc, argv);
 	return 0; 
 }
