825255dbd06530497ccaea9f6b5f4c9b1a8c6db5
Jaroslav Klaus <buzz3333@gmail.com>
2017-12-04 16:45:12 +0100

    Fix sort reverse, close #48

--- src/user/wc.cpp
+++ src/user/wc.cpp
@@ -10,48 +10,30 @@
 namespace wc_program {
 
 	size_t wc_main(int argc, char **argv) {
-		if (argc == 1 || argc == 2) {
-			bool reverse = false;
-			if (argc == 2 && (argv[1] == "/R" || argv[1] == "/r")) {
-				reverse = true;
-			}
-			std::string line = "";
-			size_t read;
-			size_t buffer_size = 255;
-			char buffer[256];
-			bool first = true;
-			while ((read = kiv_os_lib::read(buffer, buffer_size)) != -1) {
-				buffer[read] = 0; // Terminate the string.
-				if (!first) {
-					line.append(" ");
-				} else {
-					first = false;
-				}
-				line.append(buffer);
-			}
-
-			int count = 0;
-			if (!reverse) {
-				for (size_t i = 0; i < line.length(); i++) {
-					if (line[i] == ' ') {
-						count++;
-					}
-				}
+		std::string line = "";
+		size_t read;
+		size_t buffer_size = 255;
+		char buffer[256];
+		bool first = true;
+		while ((read = kiv_os_lib::read(buffer, buffer_size)) != -1) {
+			buffer[read] = 0; // Terminate the string.
+			if (!first) {
+				line.append(" ");
 			} else {
-				for (size_t i = line.length() - 1; i >= 0; i--) {
-					if (line[i] == ' ') {
-						count++;
-					}
-				}
+				first = false;
 			}
-
-			line = std::to_string(count);
-			kiv_os_lib::printLn(line.c_str(), line.length());
-		} else {
-			std::string error = "The syntax of the command is incorrect.";
-			kiv_os_lib::printErr(error.c_str(), error.length());
-			return kiv_os_lib::INCORRECT_SYNTAX;
+			line.append(buffer);
 		}
+
+		int count = 0;
+		for (size_t i = 0; i < line.length(); i++) {
+			if (line[i] == ' ') {
+				count++;
+			}
+		}
+
+		line = std::to_string(count);
+		kiv_os_lib::printLn(line.c_str(), line.length());
 
 		return kiv_os_lib::SUCCESS;
 	}
