529e0dc0605d52bc182443ea674385fef290ce9d
Thoronir42 <Thoronir42@Gmail.com>
2017-12-05 13:39:17 +0100

    Fixed DIR

--- src/user/dir.cpp
+++ src/user/dir.cpp
@@ -11,55 +11,57 @@
 namespace dir_program {
 
 	size_t dir_main(int argc, char *argv[]) {
-		if (argc >= 1 && argc <= 2) {
-			std::string path = "";
-			if (argc == 1) {
-				path = ".";
-			} else {
-				path = argv[1];
-			}
-
-			kiv_os::THandle handle;
-			const bool success = kiv_os_rtl::Create_File(path.c_str(), kiv_os::fmOpen_Always, 0, handle);
-			if (success) {
-				// Error - File does not exist.
-				std::string error = "File not found.";
-				kiv_os_lib::printErrLn(error.c_str(), error.length());
-				return kiv_os_lib::FILE_NOT_FOUND;
-			}
-
-			bool isDir;
-			uint16_t attrs;
-			bool ok = kiv_os_lib::isDir(handle, isDir, &attrs);
-			if (!ok) {
-				// Error - Get args fail.
-				std::string error = "Could not get file attributes.";
-				kiv_os_lib::printErrLn(error.c_str(), error.length());
-				return kiv_os_lib::ATTRS_ERROR;
-			}
-
-			if (isDir) {
-				size_t read;
-				kiv_os::TDir_Entry tdir;
-				bool ok;
-				while ((ok = kiv_os_rtl::Read_File(handle, &tdir, sizeof(kiv_os::TDir_Entry), read)) && read != -1) {
-					std::string s = std::to_string(tdir.file_attributes);
-					s.append("\t");
-					kiv_os_lib::print(s.c_str(), s.length());
-					kiv_os_lib::printLn(tdir.file_name, strlen(tdir.file_name));
-				}
-			} else {
-				std::string s = std::to_string(attrs);
-				s.append("\t");
-				kiv_os_lib::print(s.c_str(), s.length());
-				kiv_os_lib::printLn(path.c_str(), path.length());
-			}
-		} else {
+		if (argc < 1 || argc > 2) {
 			// Error - wrong number of parameters.
 			std::string error = "The syntax of the command is incorrect.";
 			kiv_os_lib::printErrLn(error.c_str(), error.length());
 			return kiv_os_lib::INCORRECT_SYNTAX;
 		}
+
+		std::string path = "";
+		if (argc == 1) {
+			path = ".";
+		}
+		else {
+			path = argv[1];
+		}
+
+		kiv_os::THandle handle;
+		const bool success = kiv_os_rtl::Create_File(path.c_str(), kiv_os::fmOpen_Always, kiv_os::faDirectory, handle);
+		if (!success) {
+			// Error - File does not exist.
+			std::string error = "Directory not found.";
+			kiv_os_lib::printErrLn(error.c_str(), error.length());
+			return kiv_os_lib::FILE_NOT_FOUND;
+		}
+
+		/*bool isDir;
+		uint16_t attrs;
+		bool ok = kiv_os_lib::isDir(handle, isDir, &attrs);
+		if (!ok) {
+			// Error - Get args fail.
+			std::string error = "Could not get file attributes.";
+			kiv_os_lib::printErrLn(error.c_str(), error.length());
+			return kiv_os_lib::ATTRS_ERROR;
+		}*/
+
+		size_t read;
+		kiv_os::TDir_Entry tdir;
+		bool ok = kiv_os_rtl::Read_File(handle, &tdir, sizeof(kiv_os::TDir_Entry), read);
+		while (ok && read != -1 && read != 0) {
+			std::string s = std::to_string(tdir.file_attributes);
+			s.append("\t");
+			kiv_os_lib::print(s.c_str(), s.length());
+			kiv_os_lib::printLn(tdir.file_name, strlen(tdir.file_name));
+
+			ok = kiv_os_rtl::Read_File(handle, &tdir, sizeof(kiv_os::TDir_Entry), read);
+		}
+		/*} else {
+			std::string s = std::to_string(attrs);
+			s.append("\t");
+			kiv_os_lib::print(s.c_str(), s.length());
+			kiv_os_lib::printLn(path.c_str(), path.length());
+		}*/
 
 		return kiv_os_lib::SUCCESS;
 	}
